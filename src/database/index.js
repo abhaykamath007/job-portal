f5ee20c03bc9f166d74f4c743b19e74e625bb060
abhaykamath007 <abhay.kamath100@gmail.com>
2024-08-06 23:15:46 +0530

    new commit

--- src/database/index.js
+++ src/database/index.js
@@ -6,3 +6,20 @@ const connectToDB = async()=>{
 }
 
 export default connectToDB;
+/*
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Load environment variables from .env.local file
+dotenv.config({ path: path.resolve(__dirname, '.env.local') });
+
+const connectToDB = async () => {
+    const connectionURL = process.env.MONGODB_URL;
+    mongoose.connect(connectionURL)
+        .then(() => console.log("Job portal database connection is successful"))
+        .catch(error => console.log(error));
+};
+
+export default connectToDB;
+*/
